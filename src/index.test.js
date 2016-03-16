87a1aa6494409a61c7ddf19cc760ba5025982568
Andrés Carreño <andresdavid90@gmail.com>
2016-03-15 21:37:04 -0500

    chore(releasing): Adding travis config, conventional commit, semantic release and unit testing suppo
    closes #3

---
+++ src/index.test.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect;
+var tumblrLksDownldr = require('./index.js');
+
+describe('tumblrLksDownldr', function(){
+
+  describe('setGlobalParams', function(){
+    it('should receive an object', function(){
+      expect(true).to.be.true;
+    });
+  });
+
+  describe('getLikedPosts', function(){
+    it('get the posts', function(){
+      expect(true).to.be.true;
+    });
+  });
+
+});

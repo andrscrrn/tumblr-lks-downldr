a0c14ade468d644342dc87bc51663b7a02dde1b3
Andrés Carreño <andresdavid90@Andress-MacBook-Pro.local>
2016-04-24 11:49:23 -0500

    chore(package.json): Updating dependencies
    Adding eslint support and removing unused dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  'rules': {
+    'quotes': [2, 'single'],
+    'no-nested-ternary': [1],
+    'max-statements': [1]
+  },
+  'extends': 'defaults/configurations/walmart/es6-node'
+};

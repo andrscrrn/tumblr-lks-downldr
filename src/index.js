8cdce65bc030a16f6960ba4312942c8152b7fb05
Andrés Carreño <andresdavid90@Andress-MacBook-Pro.local>
2016-04-24 11:51:49 -0500

    feat(index.js): Supporting more events
    onStart and onFetch were added so the user can now receive information when the process is starting
    and also when it is just fetching the list of images from the tumblr server

--- src/index.js
+++ src/index.js
@@ -1,239 +1,143 @@
 'use strict';
 
-/**
- * Node Dependencies
- */
-var fs = require('fs');
-var os = require('os');
-var http = require('http');
-var stdio = require('stdio');
-var progress = require('progress');
+const http = require('http');
+const pizzaGuy = require('./../../pizza-guy/src/index');
 
-/**
- * Internal Modules
- */
-var download = require('./download-image');
+const API_KEY = 'pXcUXQdlBndW7znq4C4vodeQg0OxCXOlXv2RamTphjNFj0MuzI';
 
-/**
- * Constants
- */
-var CONST = {
-  API_URL: 'api.tumblr.com',
-  API_PORT: 80,
-  API_PATH: '/v2/blog/{{blog-url}}/likes?api_key={{api-key}}{{before-param}}',
-  API_KEY: 'pXcUXQdlBndW7znq4C4vodeQg0OxCXOlXv2RamTphjNFj0MuzI',
-  DOWNLOAD_LIMIT: 20,
-  PROGRESS_BAR_FORMAT: 'Downloading: [:bar] Progress: :percent Current Image: :current Estimated Time: :etas',
-};
+let downloadedPosts = 0;
+const imagesToDownload = [];
 
-/**
- * Module Globals
- */
-var tumblrBlogUrl = '';
-var postsToLoad;
-var customPathToSave = '';
-var onEndCallback;
-
-/**
- * Vendor instances
- */
-var progressBar;
-
-/**
- * Counters
- */
-var imagesDownloaded = 0;
-var likedCount = 0;
-var postsLoadedInMemory = 0;
-var imagesThatFailed = 0;
-var currentIteration = 0;
-
-/**
- * Memory Storage
- */
-var imagesToDownload = [];
+let postsToLoad = 0;
+let tumblrBlogUrl = '';
+let customPathToSave = '';
+let onStartCallback = () => {};
+let onFetchCallback = () => {};
+let onSuccessCallback = () => {};
+let onErrorCallback = () => {};
 
 /**
  * Parse data in order to set valid global data
  * @param {Object} params object with keys
+ * @returns {void}
  */
-function setGlobalParams(params) {
-
-  params.path = params.path || '';
-  params.path = params.path.indexOf('C:\\fakepath\\') > -1
-    ? params.path.replace('C:\\fakepath\\', `${process.env.HOME}/`)
-    : params.path;
-
+const setGlobalParams = (params) => {
   tumblrBlogUrl = params.url;
-	postsToLoad = params.postsToLoad ? Number(params.postsToLoad) : postsToLoad;
+  postsToLoad = params.postsToLoad ? Number(params.postsToLoad) : postsToLoad;
   customPathToSave = params.path
     ? params.path[0] === '/'
-      ? `${params.path}/`
-      : `${process.cwd()}/${params.path}/`
-    : `${process.cwd()}/`;
+      ? `${params.path}`
+      : `${process.cwd()}/${params.path}`
+    : process.cwd();
 
-  onEndCallback = typeof params.onEnd === 'function' ? params.onEnd : function(){};
-}
+  onStartCallback = typeof params.onStart === 'function' ? params.onStart : onStartCallback;
+  onFetchCallback = typeof params.onFetch === 'function' ? params.onFetch : onFetchCallback;
+  onSuccessCallback = typeof params.onSuccess === 'function' ? params.onSuccess : onSuccessCallback;
+  onErrorCallback = typeof params.onError === 'function' ? params.onError : onErrorCallback;
+};
+
+const downloadImages = () => {
+  pizzaGuy
+    .deliver(imagesToDownload)
+    .onAddress(customPathToSave)
+    .onSuccess((info) => {
+      onSuccessCallback(info);
+    })
+    .onError((error, info) => {
+      onErrorCallback(info);
+    })
+    .start();
+};
 
 /**
  * Gets liked posts from the server
  * @param  {String} timestamp used as an offset for the request
  * @return {void}
  */
-function getLikedPosts(timestamp) {
+const getLikedPosts = (timestamp) => {
+  if (postsToLoad > 0) {
+    const beforeParam = timestamp ? `&before=${timestamp}` : '';
+    http.get(
+      {
+        host: 'api.tumblr.com',
+        port: 80,
+        path: `/v2/blog/${tumblrBlogUrl}/likes?api_key=${API_KEY}${beforeParam}`
+      },
+      (response) => {
+        let data = '';
 
-  http.get(
-    {
-      host: CONST.API_URL,
-      port: CONST.API_PORT,
-      path: CONST.API_PATH
-        .replace('{{blog-url}}', tumblrBlogUrl)
-        .replace('{{api-key}}', CONST.API_KEY)
-        .replace('{{before-param}}', timestamp ? `&before=${timestamp}` : '')
-    },
-    function(response) {
-
-      var data = '';
-
-      response.on('data', function(chunk) {
+        response.on('data', (chunk) => {
           data += chunk;
-      });
+        });
 
-      response.on('end', function() {
+        response.on('end', () => {
+          const _data = JSON.parse(data).response;
+          let likedPosts = _data.liked_posts;
+          if(likedPosts){
+            const likedCount = _data.liked_count;
+            const pushToArray = (photo) => {
+              imagesToDownload.push(
+                photo.original_size.url
+              );
+            };
+            let lastPostTimestamp = null;
 
-        var response = JSON.parse(data).response;
-        var likedPosts = response.liked_posts;
-        var lastPostTimestamp = null;
+            if (postsToLoad < likedPosts.length) {
+              likedPosts = likedPosts.slice(0, postsToLoad);
+            }
 
-        if(typeof likedPosts === 'undefined') {
-          console.log('Likes not available or not public for', tumblrBlogUrl);
-          exit();
-          return;
-        }
-
-        likedCount = response.liked_count;
-
-        // This will happen just once
-        if(!timestamp){
-
-          postsToLoad = postsToLoad
-            ? (
-                postsToLoad > likedCount
-                ? likedCount
-                : postsToLoad
-              )
-            : likedCount;
-
-          console.log('Posts to download:', postsToLoad);
-          console.log('Loading images list in memory. Please wait...');
-        }
-
-        for(var i = 0; i < likedPosts.length; i++){
-
-          lastPostTimestamp = likedPosts[i].timestamp;
-          postsLoadedInMemory++;
-
-          if(postsLoadedInMemory !== postsToLoad){
-            if(likedPosts[i].photos) {
-              likedPosts[i].photos.forEach(function(photo) {
-
-                var completeUrl = photo.original_size.url;
-                imagesToDownload.push(
-                  {
-                    'fileName': customPathToSave + completeUrl.split('/')[completeUrl.split('/').length - 1],
-                    'host': completeUrl.split('.com')[0].replace('http://', '') + '.com',
-                    'path': completeUrl.split('.com')[1]
-                  }
-                );
-                console.log(completeUrl+' ('+imagesToDownload.length+')');
+            // This will happen just once
+            if (!timestamp) {
+              postsToLoad = postsToLoad
+                ? postsToLoad > likedCount
+                  ? likedCount
+                  : postsToLoad
+                : likedCount;
+              onStartCallback({
+                postsToLoad
               });
             }
-          } else {
-            console.log('Images in memory to download:', imagesToDownload.length);
-            if(imagesToDownload.length){
-              progressBar = new progress(
-                CONST.PROGRESS_BAR_FORMAT,
-                {
-                  complete: '=',
-                  incomplete: ' ',
-                  width: 20,
-                  total: imagesToDownload.length,
-                  callback: exit
-                }
-              );
 
-              // Creating folder if it doesn't exist before start downloading
-              if (fs.existsSync(customPathToSave)){
-                queueImages();
-              } else {
-                fs.mkdirSync(customPathToSave);
-                queueImages();
+            for (let i = 0; i < likedPosts.length; i++) {
+              const currentPost = likedPosts[i];
+              if (Array.isArray(currentPost.photos)) {
+                currentPost.photos.forEach(pushToArray);
               }
-            } else {
-              // Process exit if there are no images in memory
-              exit();
+              downloadedPosts++;
+              lastPostTimestamp = likedPosts[i].timestamp;
+              if (downloadedPosts === postsToLoad) {
+                break;
+              }
             }
-            break;
+
+            onFetchCallback({
+              postsToLoad,
+              downloadedPosts,
+              imagesToDownload: imagesToDownload.length
+            });
+
+            if (postsToLoad === downloadedPosts) {
+              downloadImages();
+            } else {
+              getLikedPosts(lastPostTimestamp);
+            }
+          } else {
+            onStartCallback({
+              postsToLoad: 0
+            });
           }
-        }
-
-        // Requesting next group of liked posts
-        if(postsLoadedInMemory !== postsToLoad){
-          getLikedPosts(lastPostTimestamp);
-        }
-      });
-    }
-  )
-  .on('error', (err) => {
-    if (err) {
-      console.log('Failed loading images list on memory.');
-      throw err;
-    }
-  });
-}
-
-/**
- * Queues the request for each image on the current iteration
- * @return {void}
- */
-function queueImages() {
-
-  var i = currentIteration++ * CONST.DOWNLOAD_LIMIT;
-  var currentLimit = CONST.DOWNLOAD_LIMIT * currentIteration;
-
-  currentLimit = currentLimit > imagesToDownload.length
-    ? imagesToDownload.length
-    : currentLimit;
-
-  for(; i < currentLimit; i++){
-    download(imagesToDownload[i])
-      .then(()=>{
-        progressBar.tick(1);
-        imagesDownloaded++;
-        if(imagesDownloaded % CONST.DOWNLOAD_LIMIT === 0){
-          queueImages();
-        }
-      })
-      .catch((error)=>{
-        imagesThatFailed++;
-      });
+        });
+      }
+    )
+    .on('error', (err) => {
+      if (err) {
+        throw err;
+      }
+    });
   }
-}
-
-/**
- * Stop and exit the whole process
- * @return {void}
- */
-function exit() {
-
-  console.log('Images saved in:', customPathToSave);
-  console.log('Images that failed:', imagesThatFailed);
-  console.log('Done.');
-
-  onEndCallback();
-}
+};
 
 module.exports = {
-  'setGlobalParams': setGlobalParams,
-  'getLikedPosts': getLikedPosts
+  setGlobalParams,
+  getLikedPosts
 };
